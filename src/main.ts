8c526a3203efa8dce184a728d93b2b46d66f8d1b
BoYoung00 <qhdud20@naver.com>
2024-11-04 02:15:12 +0900

    11/04 main, shims

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import store from './store';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas, fab);
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .use(store)
+    .mount('#app');
