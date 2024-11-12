8979e0ed221343728722c28493e6060e770f3e5e
BoYoung00 <qhdud20@naver.com>
2024-11-12 16:27:28 +0900

    11/12 D-SIM 시연 동영상 링크로 변경

--- src/components/projects/DSim.vue
+++ src/components/projects/DSim.vue
@@ -115,12 +115,12 @@
     </ul>
 
     <h3>9. 시연 동영상</h3>
-    <video class="video" controls muted>
-      <source :src="require('@/assets/video/dSim.mp4')" type="video/mp4" >
-    </video>
-<!--    <ul>-->
-<!--      <li>졸업 작품회 발표를 마친 후 업로드 예정</li>-->
-<!--    </ul>-->
+<!--    <video class="video" controls muted>-->
+<!--      <source :src="require('@/assets/video/dSim.mp4')" type="video/mp4" >-->
+<!--    </video>-->
+    <ul>
+      <li><a href="https://www.youtube.com/watch?v=TCFZ6KjAqi4" target="_blank">D-SIM 시연 동영상</a></li>
+    </ul>
 
     <h3>10. 깃 주소</h3>
     <ul>
